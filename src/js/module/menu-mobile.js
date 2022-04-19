201979496d604b3587325b95b32087f388f7bfb1
Eduardo Marra <eduardomarra@gmail.com>
2022-04-19 17:43:29 -0300

    created section main NFT

--- src/js/module/menu-mobile.js
+++ src/js/module/menu-mobile.js
@@ -1,10 +1,12 @@
 export default function menuMobile() {
     const wrapperMobile = document.querySelector(".wrapper-mobile")
     const navigation = document.querySelector(".navigation")
+    const mainMobile = document.querySelector(".main")
 
     wrapperMobile.addEventListener("click", () => {
         wrapperMobile.classList.toggle('active')
         navigation.classList.toggle('active')
+        mainMobile.classList.toggle('active')
     })
 
 }
