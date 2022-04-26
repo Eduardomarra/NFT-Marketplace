a21ed68f786bf23593e10c8f045ae97d9e77c0d2
Eduardo Marra <eduardomarra@gmail.com>
2022-04-26 14:30:27 -0300

    created section Hidden Gems

--- src/js/module/menu-mobile.js
+++ src/js/module/menu-mobile.js
@@ -2,11 +2,13 @@ export default function menuMobile() {
     const wrapperMobile = document.querySelector(".wrapper-mobile")
     const navigation = document.querySelector(".navigation")
     const mainMobile = document.querySelector(".main")
+    const hiddenGems = document.querySelector(".section-hidden-gems")
 
     wrapperMobile.addEventListener("click", () => {
         wrapperMobile.classList.toggle('active')
         navigation.classList.toggle('active')
         mainMobile.classList.toggle('active')
+        hiddenGems.classList.toggle('active')
     })
 
 }
