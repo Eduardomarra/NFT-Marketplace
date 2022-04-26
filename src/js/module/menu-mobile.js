2dc95e2a1ae12afb393a6581e1cae4814e56d2a2
Eduardo Marra <eduardomarra@gmail.com>
2022-04-26 16:45:15 -0300

    created collection section

--- src/js/module/menu-mobile.js
+++ src/js/module/menu-mobile.js
@@ -3,12 +3,14 @@ export default function menuMobile() {
     const navigation = document.querySelector(".navigation")
     const mainMobile = document.querySelector(".main")
     const hiddenGems = document.querySelector(".section-hidden-gems")
+    const collection = document.querySelector(".section-collection")
 
     wrapperMobile.addEventListener("click", () => {
         wrapperMobile.classList.toggle('active')
         navigation.classList.toggle('active')
         mainMobile.classList.toggle('active')
         hiddenGems.classList.toggle('active')
+        collection.classList.toggle('active')
     })
 
 }
