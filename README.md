1bb33e85103b63fc1addd8fa9a0b2f2f2555933e
Eduardo Marra <eduardomarra@gmail.com>
2022-04-28 21:17:10 -0300

    finale configuration

---
+++ README.md
@@ -0,0 +1,40 @@
+# Portfolio
+
+<div align="center">
+  <img
+    width="768px"
+    height="auto"
+    alt="Homepage do projeto"
+    title="Home do blog"
+    src="./src/assets/NFT-Marketplace.jpg"
+  />
+</div>
+
+## 💻 Sobre o projeto
+
+---
+
+<a href="https://20-portfolio.vercel.app/" target="_blank">Versão online</a>
+
+Layout retirado da Cominidade do Figma [Figma](https://www.figma.com/file/72UK6TyIJZwzrFnIdR6mEU/NFT-Marketplace---Free-UI-Kit-Landing-Page-(Community)?node-id=0%3A1).
+
+## 🚀 Tecnologias
+
+---
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+- Deploy realizado na Vercel
+
+### Rodando localmente
+
+1. Abrir arquivo `Index.html`
+
+---
+<!-- 
+1. Instalar todos os pacotes com `yarn install`
+2. Executar o comando `yarn dev`
+3. Em alguns seguntos a URL: `http://localhost:3000/` ficará disponível -->
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-import menuMobile from './module/menu-mobile.js'
+function menuMobile() {
+  const wrapperMobile = document.querySelector(".wrapper-mobile");
+  const navigation = document.querySelector(".navigation");
+  const mainMobile = document.querySelector(".main");
+  const hiddenGems = document.querySelector(".section-hidden-gems");
+  const collection = document.querySelector(".section-collection");
+  const create = document.querySelector(".section-create");
+  const footer = document.querySelector("footer");
+
+  wrapperMobile.addEventListener("click", () => {
+    wrapperMobile.classList.toggle("active");
+    navigation.classList.toggle("active");
+    mainMobile.classList.toggle("active");
+    hiddenGems.classList.toggle("active");
+    collection.classList.toggle("active");
+    create.classList.toggle("active");
+    footer.classList.toggle("active");
+  });
+}
+
+menuMobile();
